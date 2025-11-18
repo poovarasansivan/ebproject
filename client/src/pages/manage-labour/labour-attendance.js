c1568733bf3ed8d01b42b5b7bfd90a51310e6718
poovarasansivan <poovarasansivan3@gmail.com>
2025-11-18 21:38:22 +0530

    update

--- client/src/pages/manage-labour/labour-attendance.js
+++ client/src/pages/manage-labour/labour-attendance.js
@@ -98,6 +98,7 @@ export default function LabourAttendance() {
 
             <tbody className="text-sm">
               {currentData.length > 0 ? (
+                
                 currentData.map((rec) => (
                   <tr key={rec.sno} className="border-b hover:bg-gray-50">
                     <td className="px-4 py-2">{rec.sno}</td>
